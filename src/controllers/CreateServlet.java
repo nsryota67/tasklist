d6b38bd8420658f658e0fb9c9342d8407c8e93e4
nsryo <nsryo@LAPTOP-6QQ9TF10>
2021-09-09 17:07:45 +0900

    修正

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -47,6 +47,11 @@ public class CreateServlet extends HttpServlet {
 
             em.persist(t);
             em.getTransaction().commit();
+            
+         // データベースに保存
+            em.persist(t);
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
